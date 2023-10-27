4f982f067413a7d791c80d000c81e4f1009fa0d5
nafkem <nafkem@gmail.com>
2023-10-27 18:27:44 +0100

    update my new task

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,28 +1,15 @@
-require("@nomicfoundation/hardhat-toolbox");
 
-/** @type import('hardhat/config').HardhatUserConfig */
-//require("@nomiclabs/hardhat-waffle");
-require("dotenv").config();
-const { API_URL, PRIVATE_KEY } = process.env;
+require('@nomicfoundation/hardhat-toolbox');
+require("dotenv").config({ path: ".env" });
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-const accounts = await hre.ethers.getSigners();
-for (const account of accounts) {
-   console.log(account.address);
- }
-});
 module.exports = {
- solidity: "0.8.19",
- networks: {
-  polygon_mumbai: {
-    url: "https://rpc-mumbai.maticvigil.com",
-    //@ts-ignore
-    accounts: [process.env.PRIVATE_KEY]
-  }
-},
-  etherscan: {
-  apiKey: process.env.POLYGONSCAN_API_KEY
-},
-};
+  solidity: "0.8.17",
+  networks: {
+    // for testnet
+    'base-goerli': {
+      url: 'https://goerli.base.org',
+      accounts: [process.env.PRIVATE_KEY]
+      //gasPrice: 1000000000,
+    },
+  },
+  };
